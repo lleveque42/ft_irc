1a25ace8a54f2ed50f5a81edacda9839a3db267b
lleveque42 <louis.leveque@outlook.com>
2022-11-16 14:31:40 +0100

    Client removed, user and channel added

---
+++ includes/Channel.hpp
@@ -0,0 +1,17 @@
+#ifndef CHANNEL_HPP
+# define CHANNEL_HPP
+
+class Channel
+{
+	public:
+		Channel(void);
+		Channel(** replace parameters **);
+		Channel(Channel const &instance);
+		Channel &operator=(Channel const &rhs);
+		~Channel(void);
+
+	private:
+
+};
+
+#endif
