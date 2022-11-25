abe8bc01edcaca85b3dd64b154be22cf7df9041b
lleveque42 <louis.leveque@outlook.com>
2022-11-25 18:57:40 +0100

    ERR messages ok do not open new window when wrong channel

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -6,7 +6,7 @@
 /*   By: lleveque <lleveque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 10:07:23 by arudy             #+#    #+#             */
-/*   Updated: 2022/11/25 15:55:40 by lleveque         ###   ########.fr       */
+/*   Updated: 2022/11/25 16:57:06 by lleveque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,23 +70,23 @@
 #define ERR_NOPASS ":irc.server 400 " RED "Connection refused: No password provided" RESET "\r\n"
 #define ERR_NONICK ":irc.server 400 " RED "Connection refused: No nickname provided, registration not completed" RESET "\r\n"
 #define ERR_NOUSER ":irc.server 400 " RED "Connection refused: No user informations provided, registration not completed" RESET "\r\n"
-#define ERR_NOSUCHNICK(client, nickname, target) (":" + std::string(client) + std::string(nickname) + " 401 " RED + RESET "\r\n")
-#define ERR_NOSUCHCHANNEL(client, nickname, chan_name) (":" + std::string(client) + " 403 " + std::string(nickname) + " " + RED + std::string(chan_name) + " :No such channel" RESET "\r\n")
+#define ERR_NOSUCHNICK(client, nickname, target) (":" + std::string(client) +  " 401 " + std::string(nickname) + RED " " + std::string(target) + RESET "\r\n")
+#define ERR_NOSUCHCHANNEL(client, nickname, chan_name) (":" + std::string(client) + " 403 " + std::string(nickname) + RED + " " + std::string(chan_name) + " :No such channel" RESET "\r\n")
 #define ERR_CANNOTSENDTOCHAN(client, nickname, target) (":" + std::string(client) + " 404 " RED + std::string(target) + " :Cannot send to channel" RESET "\r\n")
 #define ERR_NORECIPIENT(client, nickname) (":" + std::string(client) + " 411 " + std::string(nickname) + RED " :No recipient given (PRIVMSG)" RESET "\r\n")
 #define ERR_NOTEXTTOSEND(client, nickname) (":" + std::string(client) + " 412 " + std::string(nickname) + RED " :No text to send" RESET "\r\n")
-#define ERR_UNKNOWNCOMMAND(client, nickname, cmd) (":" + std::string(client) +" 421 " + std::string(nickname) + " " + RED + std::string(cmd) + " :Unknown command" RESET "\r\n")
-#define ERR_NONICKNAMEGIVEN(client, nickname) ":" + std::string(client) + " 431 " + std::string(nickname) +  RED " NICK :No nickname provided" RESET "\r\n"
-#define ERR_ERRONEUSNICKNAME(client, nickname) ":" + std::string(client) + " 432 " + std::string(nickname) +  RED " NICK :Erroneus nickname" RESET "\r\n"
-#define ERR_NICKNAMEINUSE(client, nickname) ":" + std::string(client) + " 433 " + std::string(nickname) +  RED " NICK :Nickname is already in use" RESET "\r\n"
-#define ERR_NOTONCHANNEL(client, nickname, chan_name) (":" + std::string(client) + " 442 " + std::string(nickname) + " " + RED + std::string(chan_name) + " :You're not on channel" RESET "\r\n"
-#define ERR_NEEDMOREPARAMS(client, nickname, cmd) (":" + std::string(client) + " 461 " + std::string(nickname) + " " + RED + std::string(cmd) + " :Not enough parameters" RESET "\r\n")
-#define ERR_NOPREFIX(client, nickname, cmd) (":" + std::string(client) + " 461 " + std::string(nickname) + " " + RED + std::string(cmd) + " :No prefix before last param" RESET "\r\n")
+#define ERR_UNKNOWNCOMMAND(client, nickname, cmd) (":" + std::string(client) +" 421 " + std::string(nickname) + RED + " " + std::string(cmd) + " :Unknown command" RESET "\r\n")
+#define ERR_NONICKNAMEGIVEN(client, nickname) ":" + std::string(client) + " 431 " + std::string(nickname) + RED " NICK :No nickname provided" RESET "\r\n"
+#define ERR_ERRONEUSNICKNAME(client, nickname) ":" + std::string(client) + " 432 " + std::string(nickname) + RED " NICK :Erroneus nickname" RESET "\r\n"
+#define ERR_NICKNAMEINUSE(client, nickname) ":" + std::string(client) + " 433 " + std::string(nickname) + RED " NICK :Nickname is already in use" RESET "\r\n"
+#define ERR_NOTONCHANNEL(client, nickname, chan_name) (":" + std::string(client) + " 442 " + std::string(nickname) + RED + " " + std::string(chan_name) + " :You're not on channel" RESET "\r\n"
+#define ERR_NEEDMOREPARAMS(client, nickname, cmd) (":" + std::string(client) + " 461 " + std::string(nickname) + RED + " " + std::string(cmd) + " :Not enough parameters" RESET "\r\n")
+#define ERR_NOPREFIX(client, nickname, cmd) (":" + std::string(client) + " 461 " + std::string(nickname) + RED + " " + std::string(cmd) + " :No prefix before last param" RESET "\r\n")
 #define ERR_ALREADYREGISTERED(client, nickname, cmd) ":" + std::string(client) + " 462 " + std::string(nickname) + RED " USER: You may not reregister" RESET "\r\n"
 #define ERR_PASSWDMISMATCH(client, nickname) ":" + std::string(client) + " 464 " + std::string(nickname) + RED " Connection refused: Password incorrect" RESET "\r\n"
-#define ERR_CHANNELISFULL(client, nickname, chan_name) (":" + std::string(client) + " 471 " + std::string(nickname) + " " + RED + std::string(chan_name) + " :Cannot join channel (+l)" RESET "\r\n")
-#define ERR_CHANOPRIVSNEEDED(client, nickname, chan_name) (":" + std::string(client) + " 482 " + std::string(nickname) + " " + RED + std::string(chan_name) + " :You're not a channel operator" RESET "\r\n")
-#define ERR_BADCHANNELKEY(client, nickname, chan_name) (":" + std::string(client) + " 475 " + std::string(nickname) + " " + RED + std::string(chan_name) + " :Cannot join channel (+k)" RESET "\r\n")
+#define ERR_CHANNELISFULL(client, nickname, chan_name) (":" + std::string(client) + " 471 " + std::string(nickname) + " " + std::string(chan_name) + " :Cannot join channel (+l)" RESET "\r\n")
+#define ERR_CHANOPRIVSNEEDED(client, nickname, chan_name) (":" + std::string(client) + " 482 " + std::string(nickname) + RED + " " + std::string(chan_name) + " :You're not a channel operator" RESET "\r\n")
+#define ERR_BADCHANNELKEY(client, nickname, chan_name) (":" + std::string(client) + " 475 " + std::string(nickname) + " " + std::string(chan_name) + " :Cannot join channel (+k)" RESET "\r\n")
 #define ERR_UMODEUNKNOWNFLAG(client, nickname) (":" + std::string(client) + " 501 " + std::string(nickname) + RED " :Uknown MODE flag" RESET "\r\n")
 #define ERR_USERSDONTMATCH(client, nickname) (":" + std::string(client) + " 502 "  + std::string(nickname) + RED " :Cant change mode for other users" RESET "\r\n")
 
