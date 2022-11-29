c3c43c23a64162b554dc7afa078389345326e9b5
lleveque42 <louis.leveque@outlook.com>
2022-11-29 13:21:41 +0100

    pong ok

--- srcs/Commands/pong.cpp
+++ srcs/Commands/pong.cpp
@@ -6,7 +6,7 @@
 /*   By: lleveque <lleveque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 11:41:05 by lleveque          #+#    #+#             */
-/*   Updated: 2022/11/21 11:41:54 by lleveque         ###   ########.fr       */
+/*   Updated: 2022/11/29 13:02:42 by lleveque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,5 @@
 
 int Server::_pong(User *user, std::string buff) {
 	(void)buff;
-	std::string msg = "PONG irc.server\r\n";
-	return _sendExecuted(user, msg);
+	return _sendExecuted(user, "PONG irc.server\r\n");
 }
