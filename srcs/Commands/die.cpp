f6adf4051bba1fec4b4bbb94caf70366de64af8a
arudy <arudy@student.42.fr>
2022-11-30 16:00:58 +0100

    die done

--- srcs/Commands/die.cpp
+++ srcs/Commands/die.cpp
@@ -6,7 +6,7 @@
 /*   By: arudy <arudy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 15:39:54 by arudy             #+#    #+#             */
-/*   Updated: 2022/11/29 16:15:16 by arudy            ###   ########.fr       */
+/*   Updated: 2022/11/30 15:36:13 by arudy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,7 @@
 int Server::_die(User *user, std::string buff) {
 	static_cast<void>(buff);
 	static_cast<void>(user);
-	// MARCHE PAS
-	// if (user->isOp())
-	// 	return 2;
-	// return _sendError(user, ERR_NOPRIVILEGES(user->getClient(), user->getNick()));
-	return 0;
+	if (user->isOp())
+		return 4;
+	return _sendError(user, ERR_NOPRIVILEGES(user->getClient(), user->getNick()));
 }
