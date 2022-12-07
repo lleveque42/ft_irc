da0deb4ba9f7f59d2ff3b3426fc1ecafd1e4aa8e
lleveque42 <louis.leveque@outlook.com>
2022-12-07 16:32:43 +0100

    quit sends message to channels

--- srcs/Commands/quit.cpp
+++ srcs/Commands/quit.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   quit.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: arudy <arudy@student.42.fr>                +#+  +:+       +#+        */
+/*   By: lleveque <lleveque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 12:38:06 by lleveque          #+#    #+#             */
-/*   Updated: 2022/12/07 15:22:43 by arudy            ###   ########.fr       */
+/*   Updated: 2022/12/07 15:53:32 by lleveque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,14 @@ int Server::_quit(User *user, std::string args) {
 		return _sendError(user, ERR_NOPREFIX(user->getClient(), user->getNick(), "QUIT"));
 	args.erase(args.begin());
 	_sendExecuted(user, RPL_QUIT((user->getClient()), args));
+	std::map<std::string, Channel *> chans(user->getJoined());
+	std::map<std::string, Channel *>::iterator ite = chans.end();
+	for (std::map<std::string, Channel *>::iterator it = chans.begin(); it != ite; it++) {
+		std::map<std::string, User *> users(it->second->getUsers());
+		for (std::map<std::string, User *>::iterator it2 = users.begin(); it2 != users.end(); it2++) {
+			if (it2->first != user->getNick())
+				_sendExecuted(it2->second, RPL_PARTMSG(user->getClient(), user->getNick(), it->first, "has left server"));
+		}
+	}
 	return _disconnectUser(user, 0);
 }
